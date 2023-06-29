57261c292c822253f5c345db051b6dc6dcd5502b
Carnage <akotoxmpimbo@gmail.com>
2023-06-28 21:43:54 -0700

    server

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const app = express();
+const cors = require('cors');
+const port = 3000
+const Stripe = require("stripe");
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const stripeRouter = require("./routes/stripe");
+const bodyParser = require('body-parser');
+
+
+dotenv.config()
+
+const stripe = Stripe(process.env.STRIPE_SECRET);
+mongoose.connect(process.env.MONGO_URL).then(() => console.log("db connected")).catch((err) => console.log(err));
+// const stripe = Stripe(process.env.STRIPE_SECRET);
+const endpointSecret = "whsec_UpAdzbPRMAp8OCOZA4RPihcReF5DYwuz";
+
+
+app.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
+    const sig = request.headers['stripe-signature'];
+  
+    let event;
+  
+    try {
+      event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+      console.log("*******signed******");
+    } catch (err) {
+      response.status(400).send(`Webhook Error: ${err.message}`);
+      return;
+    }
+  
+    // Handle the event
+    switch (event.type) {
+      case 'payment_intent.succeeded':
+        const paymentIntentSucceeded = event.data.object;
+        // Then define and call a function to handle the event payment_intent.succeeded
+        break;
+      // ... handle other event types
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+  
+    // Return a 200 response to acknowledge receipt of the event
+    response.send();
+  });
+
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+
+app.use("/stripe", stripeRouter);
+
+app.listen(process.env.PORT || port, () => console.log(`App listening on port ${process.env.PORT}!`))
---
+++ routes/stripe.js
@@ -0,0 +1,241 @@
+const express = require("express");
+const Stripe = require("stripe");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const path = require("path");
+const { time } = require("console");
+require("dotenv").config();
+
+const stripe = Stripe(process.env.STRIPE_SECRET);
+
+const router = express.Router();
+
+const checkoutSuccessPage = fs.readFileSync(
+    path.join(__dirname, 'checkout-success.html')
+  );
+  
+  router.get("/checkout-success", (req, res) => {
+    res.set("Content-Type", "text/html");
+    res.send(checkoutSuccessPage);
+  });
+
+  const checkoutCancel = fs.readFileSync(
+    path.join(__dirname, 'cancel.html')
+  );
+  
+  router.get("/cancel", (req, res) => {
+    res.set("Content-Type", "text/html");
+    res.send(checkoutCancel);
+  });
+
+
+router.post("/create-checkout-session", async (req, res) => {
+  const customer = await stripe.customers.create({
+    metadata: {
+      userId: req.body.userId,
+      cart: JSON.stringify(req.body.cartItems),
+    },
+  });
+
+ 
+  const line_items = req.body.cartItems.map((item) => {
+    return {
+      price_data: {
+        currency: "usd",
+        product_data: {
+          name: item.name,
+          // images: [item.image],
+          description: item.desc,
+          metadata: {
+            id: item.id,
+          },
+        },
+        unit_amount: item.price * 100,
+      },
+      quantity: item.cartQuantity,
+    };
+  });
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    // shipping_address_collection: {
+    //   allowed_countries: ["US", "CA", "KE"],
+    // },
+    // shipping_options: [
+    //   {
+    //     shipping_rate_data: {
+    //       type: "fixed_amount",
+    //       fixed_amount: {
+    //         amount: 0,
+    //         currency: "usd",
+    //       },
+    //       display_name: "Free shipping",
+    //       // Delivers between 5-7 business days
+    //       delivery_estimate: {
+    //         minimum: {
+    //           unit: "business_day",
+    //           value: 5,
+    //         },
+    //         maximum: {
+    //           unit: "business_day",
+    //           value: 7,
+    //         },
+    //       },
+    //     },
+    //   },
+    //   {
+    //     shipping_rate_data: {
+    //       type: "fixed_amount",
+    //       fixed_amount: {
+    //         amount: 1500,
+    //         currency: "usd",
+    //       },
+    //       display_name: "Next day air",
+    //       // Delivers in exactly 1 business day
+    //       delivery_estimate: {
+    //         minimum: {
+    //           unit: "business_day",
+    //           value: 1,
+    //         },
+    //         maximum: {
+    //           unit: "business_day",
+    //           value: 1,
+    //         },
+    //       },
+    //     },
+    //   },
+    // ],
+    
+    phone_number_collection: {
+      enabled: false,
+    },
+    line_items,
+    mode: "payment",
+    customer: customer.id,
+    success_url: "https://704f-165-154-72-184.ngrok-free.app/stripe/checkout-success",
+    cancel_url:  "https://704f-165-154-72-184.ngrok-free.app/stripe/cancel",
+  });
+
+  // res.redirect(303, session.url);
+  res.send({ url: session.url });
+});
+
+// Create order function
+
+const createOrder = async (customer, data) => {
+  const Items = JSON.parse(customer.metadata.cart);
+
+  const products = Items.map((item) => {
+    return {
+      productId: item.id,
+      quantity: item.cartQuantity,
+    };
+  });
+
+  const newOrder = new Order({
+    userId: customer.metadata.userId,
+    customerId: data.customer,
+    paymentIntentId: data.payment_intent,
+    products,
+    subtotal: data.amount_subtotal,
+    total: data.amount_total,
+    shipping: data.customer_details,
+    payment_status: data.payment_status,
+  });
+
+  try {
+    const savedOrder = await newOrder.save();
+    console.log("Processed Order:", savedOrder);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// Stripe webhoook
+
+// router.post(
+//   "/webhook",
+//   bodyParser.raw({ type: "*/*" }),
+//   async (req, res) => {
+//     // req.rawBody = buf.toString();
+//     let data;
+//     let eventType;
+
+//     // Check if webhook signing is configured.
+//     let webhookSecret;
+//     webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
+
+//     if (webhookSecret) {
+//       // Retrieve the event by verifying the signature using the raw body and secret.
+//       let event;
+//       let signature = req.headers["stripe-signature"];
+
+//       try {
+//         event = stripe.webhooks.constructEvent(
+//           req.body,
+//           signature,
+//           webhookSecret
+//         );
+//         console.log("Webhooks verified");
+//       } catch (err) {
+//         console.log(`⚠️  Webhook signature verification failed:  ${err}`);
+//         return res.sendStatus(400);
+//       }
+//       // Extract the object from the event.
+//       data = event.data.object;
+//       eventType = event.type;
+//     } else {
+//       // Webhook signing is recommended, but if the secret is not configured in `config.js`,
+//       // retrieve the event data directly from the request body.
+//       data = req.body.data.object;
+//       eventType = req.body.type;
+//     }
+
+//     // Handle the checkout.session.completed event
+//     if (eventType === "checkout.session.completed") {
+//       console.log("Session Completed");
+//       // stripe.customers
+//       //   .retrieve(data.customer)
+//       //   .then(async (customer) => {
+//       //     try {
+//       //       // CREATE ORDER
+//       //       createOrder(customer, data);
+//       //     } catch (err) {
+//       //       console.log(typeof createOrder);
+//       //       console.log(err);
+//       //     }
+//       //   })
+//       //   .catch((err) => console.log(err.message));
+//     }
+
+//     res.status(200).end();
+//   }
+// );
+
+// router.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
+//   const sig = request.headers['stripe-signature'];
+
+//   let event;
+
+//   try {
+//     event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+//   } catch (err) {
+//     response.status(400).send(`Webhook Error: ${err.message}`);
+//     return;
+//   }
+
+//   // Handle the event
+//   switch (event.type) {
+//     case 'payment_intent.succeeded':
+//       const paymentIntentSucceeded = event.data.object;
+//       // Then define and call a function to handle the event payment_intent.succeeded
+//       break;
+//     // ... handle other event types
+//     default:
+//       console.log(`Unhandled event type ${event.type}`);
+//   }
+
+//   // Return a 200 response to acknowledge receipt of the event
+//   response.send();
+// });
+
+module.exports = router;
